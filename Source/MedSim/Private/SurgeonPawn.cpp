00f4f27c89a76b8b23ceacca1a3b123053ef1aab
Andrii Kreidun <kreyduna@gmail.com>
2026-07-28 12:08:47 +0300

    SurgeonPawn stub

---
+++ Source/MedSim/Private/SurgeonPawn.cpp
@@ -0,0 +1,26 @@
+#include "SurgeonPawn.h"
+
+ASurgeonPawn::ASurgeonPawn()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASurgeonPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASurgeonPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASurgeonPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/MedSim/Public/SurgeonPawn.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "SurgeonPawn.generated.h"
+
+class UCameraComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class MEDSIM_API ASurgeonPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ASurgeonPawn();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MedSim|Camera")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MedSim|Hands")
+	UStaticMeshComponent* RightHandMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MedSim|Hands")
+	UStaticMeshComponent* LeftHandMesh;
+};
