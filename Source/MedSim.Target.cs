1d1f81bb3ed1442968c913feff7782829beb279d
Andrii Kreidun <kreyduna@gmail.com>
2026-08-27 14:35:18 +0300

    fix build

--- Source/MedSim.Target.cs
+++ Source/MedSim.Target.cs
@@ -8,7 +8,7 @@ public class MedSimTarget : TargetRules
 	public MedSimTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		bOverrideBuildEnvironment = true;
 		ExtraModuleNames.Add("MedSim");
--- Source/MedSimEditor.Target.cs
+++ Source/MedSimEditor.Target.cs
@@ -8,7 +8,7 @@ public class MedSimEditorTarget : TargetRules
 	public MedSimEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		bOverrideBuildEnvironment = true;
 		ExtraModuleNames.Add("MedSim");
