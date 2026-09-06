3a77fff6ee4b4b1f4aaf7b1850648683a858f83f
Andrii Kreidun <kreyduna@gmail.com>
2026-09-06 13:06:18 +0300

    remove check intersection on tet edges

--- Source/MedSim/Public/Tissue/Geometry/TissueCutSurface.h
+++ Source/MedSim/Public/Tissue/Geometry/TissueCutSurface.h
@@ -3,38 +3,6 @@
 #include "CoreMinimal.h"
 #include "Tissue/Geometry/SweptBladeIntersection.h"
 
-struct FTetBoundaryIntersection
-{
-	/**
-	 * Index of Tet edge:
-	 *
-	 * 0 = V0-V1
-	 * 1 = V0-V2
-	 * 2 = V0-V3
-	 * 3 = V1-V2
-	 * 4 = V1-V3
-	 * 5 = V2-V3
-	 */
-	int32 TetEdgeIndex = INDEX_NONE;
-
-	/**
-	 * Intersection point in Tissue Local Space.
-	 */
-	FVector3f Point = FVector3f::ZeroVector;
-
-	/**
-	 * Parametric position along Tet edge.
-	 * 0 = edge start
-	 * 1 = edge end.
-	 */
-	float EdgeT = 0.0f;
-
-	/**
-	 * Swept triangle that produced this intersection.
-	 */
-	int32 BladeTriangleIndex = INDEX_NONE;
-};
-
 struct FTetCutPatch
 {
 	int32 BladeTriangleIndex = INDEX_NONE;
@@ -52,11 +20,17 @@ struct FTetCutData
 
 	TArray<FTetCutPatch> Patches;
 
-	TArray<FTetBoundaryIntersection> BoundaryIntersections;
-
 	float TotalIntersectionArea = 0.0f;
 
 	bool bNeedsCut = false;
+};
+
+struct FTetCutSurface
+{
+	int32 TetId;
+
+	TArray<FVector3f> Vertices;
+	TArray<FIntVector3> Triangles;
 };
 
 // Result of intersections
@@ -65,9 +39,4 @@ namespace TissueCutSurface
 	float ComputePolygonArea(const TArray<FVector3f>& Polygon);
 
 	void BuildTetCutData(const TArray<FTriangleTetIntersection>& Intersections, TArray<FTetCutData>& OutTetCuts);
-
-	void FindTetEdgeIntersections(
-		const TArray<FSweptBladeTriangle>& SweptTriangles,
-		const FTissueTopologySnapshot& Snapshot,
-		FTetCutData& TetCut);
 }
